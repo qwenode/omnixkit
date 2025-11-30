e2bcd48dd86f05613b1213249cafa891c5a9a9b7
root <dtfreemandev@gmail.com>
2025-12-01 02:27:14 +0800

    feat(pagination): 添加默认最大分页大小支持

--- kitset/page_size.go
+++ kitset/page_size.go
@@ -6,7 +6,8 @@ type PageSize interface {
     SetSize(size int32)
     SetPage(page int32)
 }
-
+// 默认的最大分页大小,允许修改
+var DefaultMaxSize int32 = 1000
 // 根据分页参数计算 offset 和 limit
 // page 从 1 开始，size 为每页数量
 // 返回值用于数据库查询的 OFFSET 和 LIMIT
@@ -17,14 +18,14 @@ func PageToOffsetLimit(input PageSize) (_offset int, _limit int) {
         page = 1
     }
     if size < 1 {
-        size = 20 // 默认每页 10 条
+        size = 20
     }
     return (page - 1) * size, size
 }
 // 根据分页参数计算 offset 和 limit，限制最大 size 为 1000
 func PageToOffsetLimitDefault(input PageSize) (_offset int, _limit int) {
-    if input.GetSize() > 1000 {
-        input.SetSize(1000)
+    if input.GetSize() > DefaultMaxSize {
+        input.SetSize(DefaultMaxSize)
     }
     return PageToOffsetLimit(input)
 }
